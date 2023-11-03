7529c979a520a90ed818e0ffacd13efd48b65364
ahmed <ahmedbardesy95@gmail.com>
2023-11-03 20:42:29 +0200

    create custome note item

---
+++ lib/views/custom_note_item.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+
+class CustomNoteItem extends StatelessWidget {
+  const CustomNoteItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 14, bottom: 24, left: 16),
+      decoration: BoxDecoration(
+          color: Colors.orange, borderRadius: BorderRadius.circular(16)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Padding(
+              padding: EdgeInsets.only(bottom: 15),
+              child: Text(
+                'Flutter Tips',
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+            ),
+            subtitle: Text('Ahmed khaled is the best software engeiner',
+                style: TextStyle(color: Colors.black.withOpacity(.6))),
+            trailing: IconButton(
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                ),
+                onPressed: () {}),
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Text('May 21,2022',
+                style: TextStyle(color: Colors.black.withOpacity(.4))),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/views/homeView.dart
+++ lib/views/homeView.dart
@@ -1,19 +1,50 @@
 import 'package:flutter/material.dart';
 
+import 'custom_note_item.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        actions: [Icon(Icons.search)],
-        title: Text(
-          'Notes',
-          style: TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+              child: Row(
+                children: [
+                  const Text(
+                    'Notes',
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  const Spacer(),
+                  Container(
+                    height: 45,
+                    width: 45,
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(.05),
+                        borderRadius: BorderRadius.circular(15)),
+                    child: const Icon(Icons.search),
+                  )
+                ],
+              ),
+            ),
+            const NotesItem()
+          ],
         ),
       ),
     );
+  }
+}
+
+class NotesItem extends StatelessWidget {
+  const NotesItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomNoteItem();
   }
 }
